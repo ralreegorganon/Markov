cf8896a9db85d55f1cbb8791e867b0f7cbe583d9
Jason Jones <jasonedwardjones@gmail.com>
2009-08-07 18:56:17 -0800

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Markov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText(@"c:\users\jj\desktop\architecture.txt");
+            var seed = input.Tokenize();
+            var chain = new Chain<string>(seed, 3);
+            chain.Generate("I", 2000).ForEach(Console.Write);
+        }
+    }
+
+    public static class JExtensions
+    {
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (T element in source) action(element);
+            return source;
+        }
+
+        public static IEnumerable<string> Tokenize(this string source)
+        {
+            var regex = new Regex(@"(\W+)");
+            return new List<string>(regex.Split(source));
+        }
+    }
+}
